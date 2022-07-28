ea09bdc0aef226f9c85d8681944d869a50c5b419
Alex Westphal <awestphal@onpointlearning.com>
2022-07-28 19:44:07 +1200

    Improved the UserList search component

--- src/main/typescript/frontend/user-list/components/UserList.tsx
+++ src/main/typescript/frontend/user-list/components/UserList.tsx
@@ -9,45 +9,50 @@ import {
     Button,
     Checkbox,
     ClickAwayListener,
-    Container,
+    Container, Dialog, DialogActions, DialogContent, DialogTitle, Divider,
     IconButton,
-    InputAdornment,
+    InputBase,
     List,
     ListItem,
     ListItemButton,
     ListItemIcon,
     ListItemText,
-    ListSubheader,
-    Menu,
-    MenuItem,
-    outlinedInputClasses,
+    ListSubheader, MenuItem, OutlinedInput,
     Paper,
-    Popper,
+    Popper, Select, SelectChangeEvent,
     Table,
     TableBody,
     TableCell,
     TableContainer,
     TableHead,
-    TableRow,
-    TextField
+    TableRow, tableRowClasses, Tooltip,
 } from '@mui/material'
 import {blueGrey, grey} from '@mui/material/colors'
 
-import CheckBoxOutlineBlankIcon from '@mui/icons-material/CheckBoxOutlineBlank'
+import AddIcon from '@mui/icons-material/Add'
 import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import FilterIcon from '@mui/icons-material/FilterAlt'
 import SearchIcon from '@mui/icons-material/Search'
-import MoreVertIcon from '@mui/icons-material/MoreVert'
 
 import {cls, createClasses} from '@axwt/util'
 
 import {axwtClasses} from '@axwt/core'
 
-import {Users} from '../data'
+import {getUser, Orgs, Users} from '../data'
 
 
-export const userListClasses = createClasses("UserList", ["container", "header", "searchBox", "tableHeadCell", "tableHeadCellInner", "tableHeadCellMoreIcon", "tableHeadCellText", "title"])
+export const userListClasses = createClasses("UserList", ["container", "header", "searchBoxContainer", "tableHeadCell", "tableHeadCellInner", "tableHeadCellMoreIcon", "tableHeadCellText", "title"])
 
 export const UserList: React.FC = () => {
+
+    const [shownRoles, setShownRoles] = React.useState<{ [key: string]: boolean }>({
+        Users: true, Managers: true, Records: true, Instructors: true, Testers: true
+    })
+    const [shownStatuses, setShownStatuses] = React.useState<{ [key: string]: boolean }>({
+        Active: true, Inactive: true, Record: true
+    })
+    const [shownOrgs, setShownOrgs] = React.useState<string[]>([])
+
 
     const classes = userListClasses
 
@@ -65,61 +70,52 @@ export const UserList: React.FC = () => {
                 display: 'flex',
                 alignItems: 'center'
             },
-            [`& .${classes.searchBox}`]: {
+            [`& .${classes.searchBoxContainer}`]: {
+                position: 'relative',
                 flexGrow: 1,
                 marginX: 2,
-                backgroundColor: 'background.paper',
-
-                [`& .${outlinedInputClasses.input}`]: {
-                    paddingY: 1
-                },
+                alignSelf: 'start',
             },
 
             [`& .${classes.title}`]: {
                 typography: 'h6',
                 marginRight: 4
             },
+
+            [`& .${tableRowClasses.root}:not(.${tableRowClasses.head})`]: {
+
+                "&:hover": {
+                    backgroundColor: 'action.hover'
+                }
+            }
         }}
     >
         <Container className={classes.container}>
             <div className={classes.header}>
                 <div className={classes.title}>Users & Groups: Users</div>
-                <TextField
-                    className={classes.searchBox}
-                    variant="outlined"
-                    placeholder="Search"
-                    autoFocus
-                    InputProps={{
-                        endAdornment: <InputAdornment position="end">
-                            <IconButton>
-                                <SearchIcon/>
-                            </IconButton>
-                            <IconButton edge="end">
-                                <MoreVertIcon/>
-                            </IconButton>
-                        </InputAdornment>
-                    }}
-                />
-                <Button variant="contained">Add User</Button>
+                <div className={classes.searchBoxContainer}>
+                    <SearchBox/>
+                </div>
+                <Button variant="contained" startIcon={<AddIcon/>}>Add User</Button>
             </div>
 
             <TableContainer component={Paper}>
                 <Table size="small">
                     <TableHead>
                         <TableRow>
-                            <TableCell>ID</TableCell>
+                            <TableCell align="right">User ID</TableCell>
                             <TableCell>User Name</TableCell>
                             <TableCell>User Login</TableCell>
-                            <TableCell>Organization</TableCell>
-                            <ColumnFilterHeadCell columnName="Role" values={["Manager", "User", "Instructor", "Tester"]}/>
+                            <ColumnSelectHeadCell columnName="Organization" possibleValues={Orgs} selectedValues={shownOrgs} onChange={setShownOrgs}/>
+                            <ColumnFilterHeadCell columnName="Role" values={shownRoles} onChange={setShownRoles}/>
 
-                            <ColumnFilterHeadCell columnName="Status" values={["Active", "Inactive", "Record"]}/>
+                            <ColumnFilterHeadCell columnName="Status" values={shownStatuses} onChange={setShownStatuses}/>
                         </TableRow>
                     </TableHead>
                     <TableBody>
                         {Users.map(user =>
                             <TableRow key={user.userId}>
-                                <TableCell>{user.userId}</TableCell>
+                                <TableCell align="right">{user.userId}</TableCell>
                                 <TableCell>{`${user.lastName}, ${user.firstName}`}</TableCell>
                                 <TableCell>{user.login}</TableCell>
                                 <TableCell>{user.org}</TableCell>
@@ -131,25 +127,155 @@ export const UserList: React.FC = () => {
                 </Table>
             </TableContainer>
         </Container>
+
     </Box>
 }
 
 export default UserList
 
 
+interface SearchBoxProps {
+
+}
+
+const searchBoxClasses = createClasses("SearchBox", ["divider", "focus", "input", "main", "recentList", "recentList_id", "targetField", "verticalDivider"])
+
+const SearchBox: React.FC<SearchBoxProps> = ({}) => {
+
+    const [filterDialogOpen, setFilterDialogOpen] = React.useState<boolean>(false)
+    const [searchText, setSearchText] = React.useState<string>("")
+    const [targetField, setTargetField] = React.useState<string>("last_name")
+
+    const [hasFocus, setHasFocus] = React.useState<boolean>(false)
+
+    const handleChange: React.ChangeEventHandler<HTMLInputElement> = (ev) => {
+        setSearchText(ev.target.value.trimStart())
+    }
+
+    const classes = searchBoxClasses
+
+    return <>
+        <ClickAwayListener onClickAway={() => setHasFocus(false)}>
+            <Paper
+                className={cls(classes.root, { [classes.focus]: hasFocus })}
+                component="form"
+                elevation={hasFocus ? 1 : 0}
+                onFocus={() => setHasFocus(true)}
+                sx={{
+                    width: '100%',
+                    position: 'absolute',
+
+                    padding: '1px 2px',
+                    border: 1,
+                    borderColor: grey[400],
+
+                    "&:hover": {
+                        borderColor: 'primary.main',
+                    },
+
+                    [`&.${classes.focus}`]: {
+                        padding: '0px 1px',
+                        border: 2,
+                        borderColor: 'primary.main',
+                    },
+                    [`& .${classes.divider}`]: {
+                        width: '100%',
+                    },
+                    [`& .${classes.input}`]: {
+                        marginLeft: 1,
+                        flex: 1,
+                    },
+                    [`& .${classes.main}`]: {
+                        display: 'flex',
+                        alignItems: 'center',
+                    },
+                    [`& .${classes.recentList}`]: {
+
+                    },
+                    [`& .${classes.recentList_id}`]: {
+                        display: 'inline-block',
+                        width: '4em',
+                        marginRight: 2,
+                    },
+                    [`& .${classes.targetField}`]: {
+                        marginX: 2, marginY: 1
+                    },
+                    [`& .${classes.verticalDivider}`]: {
+                        height: 28,
+                        margin: 1/2,
+                    },
+                }}
+            >
+                <div className={classes.main}>
+                    <InputBase
+                        className={classes.input}
+                        placeholder="Search Users"
+                        value={searchText}
+                        onChange={handleChange}
+                    />
+                    <Tooltip title="Do Search">
+                        <IconButton size="small">
+                            <SearchIcon/>
+                        </IconButton>
+                    </Tooltip>
+                    <Divider className={classes.verticalDivider} orientation="vertical" />
+                    <Tooltip title="Filter Options">
+                        <IconButton size="small" onClick={() => setFilterDialogOpen(true)}>
+                            <FilterIcon/>
+                        </IconButton>
+                    </Tooltip>
+                </div>
+                {hasFocus && <>
+                    <Divider orientation="horizontal"/>
+                    <List
+                        className={classes.recentList}
+                        dense
+                        subheader={<ListSubheader disableGutters sx={{ pl: 1 }}>Recent</ListSubheader>}
+                    >
+                        {[12022,12033].map(getUser).map(user =>
+                            <ListItem key={user.userId} disableGutters disablePadding>
+                                <ListItemButton>
+                                    <ListItemText>
+                                        <span className={classes.recentList_id}>{user.userId}</span>
+                                        {`${user.lastName}, ${user.firstName}`}
+                                    </ListItemText>
+                                </ListItemButton>
+                            </ListItem>
+                        )}
+                    </List>
+                </>}
+            </Paper>
+        </ClickAwayListener>
+        <Dialog open={filterDialogOpen} onClose={() => setFilterDialogOpen(false)}>
+            <DialogTitle>Filter Options</DialogTitle>
+            <DialogContent></DialogContent>
+            <DialogActions>
+                <Button onClick={() => setFilterDialogOpen(false)}>Apply</Button>
+            </DialogActions>
+        </Dialog>
+    </>
+}
+
+
 interface ColumnFilterHeadCellProps {
     columnName: string
-    values: string[]
+    values: { [key: string]: boolean }
+    onChange: (newValues: { [key: string]: boolean }) => void
 }
 
 const columnFilterHeadCellClasses = createClasses("ColumnFilterHeadCell", ["inner", "moreIcon", "text"])
 
-const ColumnFilterHeadCell: React.FC<ColumnFilterHeadCellProps> = ({columnName, values}) => {
+const ColumnFilterHeadCell: React.FC<ColumnFilterHeadCellProps> = ({columnName, values, onChange}) => {
 
     const [popperAnchorEl, setPopperAnchorEl] = React.useState<HTMLElement | null>(null)
 
-    const handleClick: React.MouseEventHandler<HTMLElement> = (ev) => {
+    const handleCellClick: React.MouseEventHandler<HTMLElement> = (ev) => {
         setPopperAnchorEl(popperAnchorEl ? null : ev.currentTarget)
+    }
+
+    const handleToggle = (value: string) => () => {
+        let newValues = { ...values, [value]: !values[value] }
+        onChange(newValues)
     }
 
     const classes = columnFilterHeadCellClasses
@@ -158,7 +284,7 @@ const ColumnFilterHeadCell: React.FC<ColumnFilterHeadCellProps> = ({columnName, 
 
         <TableCell
             className={classes.root}
-            onClick={handleClick}
+            onClick={handleCellClick}
             sx={{
 
                 [`& .${classes.inner}`]: {
@@ -192,20 +318,24 @@ const ColumnFilterHeadCell: React.FC<ColumnFilterHeadCellProps> = ({columnName, 
                 open={Boolean(popperAnchorEl)}
                 onClick={ev => ev.stopPropagation()}
                 anchorEl={popperAnchorEl}
-                placement="bottom-end"
+                placement="bottom-start"
+                sx={{
+                    width: popperAnchorEl ? popperAnchorEl.clientWidth : 'unset'
+                }}
             >
                 <Paper square>
                     <List
-                        subheader={<ListSubheader>Show Only</ListSubheader>}
+                        subheader={<ListSubheader disableGutters sx={{ pl: 1 }}>Show Only</ListSubheader>}
                     >
-                        { values.map(value =>
+                        { Object.keys(values).map(value =>
                             <ListItem key={value} disablePadding>
-                                <ListItemButton dense>
+                                <ListItemButton role={undefined} onClick={handleToggle(value)} dense>
                                     <ListItemIcon>
                                         <Checkbox
                                             edge="start"
                                             tabIndex={-1}
                                             disableRipple
+                                            checked={values[value]}
                                         />
                                     </ListItemIcon>
                                     <ListItemText>{value}</ListItemText>
@@ -213,6 +343,90 @@ const ColumnFilterHeadCell: React.FC<ColumnFilterHeadCellProps> = ({columnName, 
                             </ListItem>
                         )}
                     </List>
+                </Paper>
+            </Popper>
+        </TableCell>
+    </ClickAwayListener>
+}
+
+
+interface ColumnSelectHeadCellProps {
+    columnName: string
+    possibleValues: string[]
+    selectedValues: string[]
+    onChange: (newValues: string[]) => void
+}
+
+const columnSelectHeadCellClasses = createClasses("ColumnSelectHeadCell", ["inner", "moreIcon", "text"])
+
+const ColumnSelectHeadCell: React.FC<ColumnSelectHeadCellProps> = ({columnName, possibleValues, selectedValues, onChange}) => {
+
+    const [popperAnchorEl, setPopperAnchorEl] = React.useState<HTMLElement | null>(null)
+
+    const handleCellClick: React.MouseEventHandler<HTMLElement> = (ev) => {
+        setPopperAnchorEl(popperAnchorEl ? null : ev.currentTarget)
+    }
+
+    const handleChange = (event: SelectChangeEvent) => {
+        onChange(event.target.value as unknown as string[])
+    }
+
+    const classes = columnSelectHeadCellClasses
+
+    return <ClickAwayListener onClickAway={() => setPopperAnchorEl(null)}>
+
+        <TableCell
+            className={classes.root}
+            onClick={handleCellClick}
+            sx={{
+
+                [`& .${classes.inner}`]: {
+                    display: 'flex',
+                    alignItems: 'center',
+                },
+
+                [`& .${classes.moreIcon}`]: {
+                    visibility: 'none'
+                },
+
+                [`& .${classes.text}`]: {
+                    flexGrow: 1
+                },
+
+                "&:hover": {
+                    backgroundColor: grey[100],
+                    cursor: 'pointer',
+
+                    [`& .${classes.moreIcon}`]: {
+                        visibility: 'visible'
+                    },
+                },
+            }}
+        >
+            <div className={classes.inner}>
+                <div className={classes.text}>{columnName}</div>
+                <ExpandMoreIcon className={classes.moreIcon} fontSize="small"/>
+            </div>
+            <Popper
+                open={Boolean(popperAnchorEl)}
+                onClick={ev => ev.stopPropagation()}
+                anchorEl={popperAnchorEl}
+                placement="bottom-start"
+                sx={{
+                    width: popperAnchorEl ? popperAnchorEl.clientWidth : 'unset'
+                }}
+            >
+                <Paper square>
+                    <Select
+                        multiple fullWidth
+                        value={selectedValues as any}
+                        onChange={handleChange}
+                        input={<OutlinedInput label={columnName}/>}
+                    >
+                        {possibleValues.map(value =>
+                            <MenuItem key={value} value={value}>{value}</MenuItem>
+                        )}
+                    </Select>
                 </Paper>
             </Popper>
         </TableCell>
--- src/main/typescript/frontend/user-list/data/Users.ts
+++ src/main/typescript/frontend/user-list/data/Users.ts
@@ -1,12 +1,43 @@
 import {User} from './User'
+import {ArrayUtils} from '@axwt/util'
 
 export const Users: User[] = [
     {
-        userId: 2, firstName: 'Amos', lastName: 'Burton', login: 'amos.burton',
+        userId: 12022, firstName: 'Amos', lastName: 'Burton', login: 'amos.burton',
         org: "Customer A", role: 'User', status: 'Active'
     },
     {
-        userId: 3, firstName: 'James', lastName: 'Holden', login: 'james.holden',
+        userId: 12033, firstName: 'James', lastName: 'Holden', login: 'james.holden',
         org: "Customer A", role: 'Manager', status: 'Active'
-    }
+    },
+    {
+        userId: 12044, firstName: 'Naomi', lastName: 'Nagata', login: 'naomi.nagata',
+        org: "Customer A", role: 'User', status: 'Active'
+    },
+    {
+        userId: 12055, firstName: 'Alex', lastName: 'Kamal', login: 'alex.kamal',
+        org: "Customer A", role: 'User', status: 'Active'
+    },
+    {
+        userId: 12066, firstName: 'Klaes', lastName: 'Ashford', login: 'klaes.ashford',
+        org: "Customer A", role: 'Manager', status: 'Active'
+    },
+    {
+        userId: 12077, firstName: 'Camina', lastName: 'Drummer', login: 'camina.drummer',
+        org: "Customer A", role: 'User', status: 'Active'
+    },
+    {
+        userId: 12088, firstName: 'Anderson', lastName: 'Dawes', login: 'anderson.dawes',
+        org: "Customer A", role: 'Manager', status: 'Active'
+    },
 ]
+
+export const Orgs = ArrayUtils.distinct(Users.map(user => user.org))
+
+export const getUser = (userId: number): User => {
+
+    for(let i=0; i<Users.length; i++) {
+        if(Users[i].userId == userId) return Users[i]
+    }
+    return null
+}
